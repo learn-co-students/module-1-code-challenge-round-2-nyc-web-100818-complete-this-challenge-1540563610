7fba2238f26da73eca61daf15b333573dcf68f18
magic-onion <alexander.m.truong@gmail.com>
2018-10-26 11:42:01 -0400

    cool beans

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,4 +1,5 @@
 class Viewer
+
   attr_accessor :username
 
   @@all = []
@@ -11,5 +12,28 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+  def queue_items
+    QueueItem.all.select {|item| item.viewer == self}
+  end
+
+  def queue_movies
+    self.queue_items.map {|item| item.movie}
+  end
+
+  def add_movie_to_queue(movie)
+    QueueItem.new(self, movie)
+  end
+
+  #had some trouble constraining rating value.
+  def rate_movie(movie, rating)
+    my_movie = QueueItem.find_item(self, movie)
+    if my_movie != nil
+      my_movie.rating = rating
+    else
+      new_queue_item = self.add_movie_to_queue(movie)
+      new_queue_item.rating = rating
+    end
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,42 @@ def reload
   load 'config/environment.rb'
 end
 
+
+ alex = Viewer.new("alex")
+ jimmy = Viewer.new("jimmy")
+ ronny = Viewer.new("ronny")
+ cheesy = Viewer.new("cheesy")
+
+ death = Movie.new("death")
+ spooky = Movie.new("spooky")
+ wasted = Movie.new("wasted")
+ rime = Movie.new("rime")
+ crust = Movie.new("crust")
+ lament = Movie.new("lament")
+ fkn_metal = Movie.new("fkn metal")
+
+ qi1 = QueueItem.new(jimmy, lament)
+ qi2 = QueueItem.new(cheesy, crust)
+ qi3 = QueueItem.new(ronny, fkn_metal)
+ qi4 = QueueItem.new(jimmy, wasted)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
