bdee0b97f6765919fdafcefdb26d62b71194fa6a
no0ro <nrolwes@gmail.com>
2018-10-26 11:41:53 -0400

    done

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,4 +1,40 @@
-class QueueItem
+class QueueItem # an instance of a queue
+  attr_accessor :viewer, :movie
 
+  @@all = []
 
-end
+  def initialize(viewer, movie)
+    @movie = movie
+    @viewer = viewer
+
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def rating
+  end
+end 
+
+  # returns the rating for this `QueueItem`. If the viewer has not yet rated the movie, `QueueItem#rating` should be `nil`
+
+#
+# #_A `QueueItem` object represents that an individual user has added a particular movie to their queue,
+# #and possibly rated it. **It is not the full queue, but a single item in the queue.**_
+# #
+#
+#
+# ___??
+#
+=begin
++ `QueueItem.all`
+  + returns an array of all `QueueItem`s
++ `QueueItem#viewer`
+  + returns the viewer associated with this `QueueItem`
++ `QueueItem#movie`
+  + returns the movie associated with this `QueueItem`
++ `QueueItem#rating`
+  + returns the rating for this `QueueItem`. If the viewer has not yet rated the movie, `QueueItem#rating` should be `nil`
+=end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,26 @@ def reload
   load 'config/environment.rb'
 end
 
+
+v1 = Viewer.new ("noelle")
+v2 = Viewer.new ("molly")
+v3 = Viewer.new ("sarah")
+v4 = Viewer.new ("dave")
+v5 = Viewer.new ("faith")
+
+
+m1 = Movie.new("mov1")
+m2 = Movie.new("mov2")
+m3 = Movie.new("mov3")
+m4 = Movie.new("mov4")
+
+qi1 = QueueItem.new(v1, m4)
+qi2 = QueueItem.new(v2, m2)
+qi3 = QueueItem.new(v4, m1)
+qi4 = QueueItem.new(v5, m3)
+qi5 = QueueItem.new(v2, m4)
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
