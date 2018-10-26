6860a2d9e0d6626c6e7eef50744fc76d0d888ff1
github username <github email address>
2018-10-26 11:40:28 -0400

    retry 2

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,41 @@ class Viewer
   def self.all
     @@all
   end
-  
-end
+
+  def queue_items
+    # + this method should return an array of `QueueItem` instances associated
+    # with this instance of `Viewer`.
+    QueueItem.all.select do |qi|
+      qi.viewer == self
+    end
+  end
+
+  def queue_movies
+    # this method should return an array of `Movie` instances in the `Viewer`'s queue.
+    queue_items.map do |qi|
+      qi.movie
+    end
+  end
+
+  def add_movie_to_queue(movie)
+    # + this method should receive a `Movie` instance as its only argument and
+    # add it to the `Viewer`'s queue
+    QueueItem.new(self, movie)
+  end
+
+  def rate_movie(movie, rating)
+    # + given a movie and a rating (a number between 1 and 5), this method
+    # should assign the rating to the viewer's `QueueItem` for that movie.
+    # If the movie is not already in the viewer's queue,
+    # this method should add a new `QueueItem` with the viewer, movie, and rating.
+
+    # if self.add_movie_to_queue(movie) == nil
+    #   QueueItem.new(self, movie, rating)
+
+    if queue_movies == nil
+      QueueItem.new(self, movie, rating)
+    # If the movie is already in the queue, this method should not create a new `QueueItem`.
+    end
+    queue_movies
+  end
+end #end of class
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,18 @@ def reload
   load 'config/environment.rb'
 end
 
+m1 = Movie.new("m1")
+m2 = Movie.new("m2")
+m3 = Movie.new("m3")
+
+v1 = Viewer.new("v1")
+v2 = Viewer.new("v2")
+v3 = Viewer.new("v3")
+
+qi1 = QueueItem.new(v1, m1, 2)
+qi2 = QueueItem.new(v2, m2, 0)
+qi3 = QueueItem.new(v3, m3, 3)
+qi4 = QueueItem.new(v1, m1, 5)
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
