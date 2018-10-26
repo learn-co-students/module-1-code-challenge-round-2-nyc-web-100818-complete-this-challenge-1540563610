785a9469bd208d13e00e4fdbc4e7979e5bcbdce2
github username <github email address>
2018-10-26 11:41:36 -0400

    Done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,16 @@
+### `Movie`
+
+# + `Movie.all`
+#   + returns an array of all `Movie`
+# + `Movie#queue_items`
+#   + returns an array of all the `QueueItem` instances that contain this movie
+# + `Movie#viewers`
+#   + returns an array of all of the `Viewer`s with this `Movie` instance in their queue
+# + `Movie#average_rating`
+#   + returns the average of all ratings for this instance of `Movie`
+# + `Movie.highest_rated`
+#   + returns the instance of `Movie` with the highest average rating
+
 class Movie
   attr_accessor :title
 
@@ -11,5 +24,35 @@ class Movie
   def self.all
     @@all
   end
+
+  def queue_items
+    # returns an array of all the `QueueItem` instances that contain this movie
+    QueueItem.all.select do |queueItem|
+      queueItem.movie == self
+    end
+  end
+
+  def viewers
+    #returns an array of all of the `Viewer`s with this `Movie` instance in their queue
+    self.queue_items.map do |queueItem|
+      queueItem.viewer
+    end
+  end
+
+  def average_rating
+    #returns the average of all ratings for this instance of `Movie`
+    r = self.queue_items.map do |queueItem|
+      queueItem.rating
+    end
+    r.reduce(:+)/r.count
+  end
+
+  def self.highest_rated
+    #returns the instance of `Movie` with the highest average rating
+    Movie.all.max_by do |element|
+      element.title
+    end
+  end
+
 
 end
